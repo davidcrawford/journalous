3bfc5dc2e375c02c1776f27df338f23c34a150dc
David Crawford <david.crawford@gmail.com>
2010-08-06 09:42:43 -0400

    Add user model

--- Gemfile
+++ Gemfile
@@ -1,6 +1,14 @@
 source "http://rubygems.org"
 
+gem 'rails', '2.3.8'
+gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
+
 group :development do
   gem 'rspec-rails', '2.0.0.beta.18'
   gem 'annotate-models'
 end
+
+group :test do
+  gem 'rspec', '2.0.0.beta.18'
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100729042959) do
+ActiveRecord::Schema.define(:version => 20100806055616) do
 
   create_table "prompts", :force => true do |t|
     t.string   "text"
     t.string   "quote"
     t.string   "quote_author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
