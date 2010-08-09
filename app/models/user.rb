db9fa50c41620bbcdb71736b56f72adad75c5b5b
David Crawford <david.crawford@gmail.com>
2010-08-08 23:59:36 -0400

    Add password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,48 @@
 #  updated_at :datetime
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
-    attr_accessible :name, :email
+    attr_accessor :password
+    attr_accessible :name, :email, :password, :password_confirmation
     
-    validates_presence_of :name
-    validates_presence_of :email
+    validates_presence_of :name, :email, :password
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
     validates_uniqueness_of :email
+    validates_confirmation_of :password
+    validates_length_of :password, :in => 6..40
+
+    before_save :encrypt_password
+
+    # Return true if the password matches the submitted password
+    def has_password?(submitted)
+      # Compare encrypted version of the password with encrypted submitted password
+      encrypt(submitted) == encrypted_password
+    end
+
+    def self.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+
+    private
+
+      def encrypt_password
+        self.salt = make_salt if new_record?
+        self.encrypted_password = encrypt(password)
+      end
+      
+      def encrypt(string)
+        secure_hash("#{salt}--#{string}")
+      end
+
+      def make_salt
+        secure_hash("#{Time.now.utc}--#{password}")
+      end
+
+      def secure_hash(string)
+        Digest::SHA2.hexdigest(string)
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100808042545) do
+ActiveRecord::Schema.define(:version => 20100809032753) do
 
   create_table "prompts", :force => true do |t|
     t.string   "text"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(:version => 20100808042545) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
