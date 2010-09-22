0be9682748ef2716ab03846d06c515168f03ca34
David Crawford <david.crawford@gmail.com>
2010-09-22 01:15:36 -0400

    Get rid of silly code that never did anything.

--- public/javascripts/jquery.autosubmit.js
+++ public/javascripts/jquery.autosubmit.js
@@ -1,37 +1,16 @@
 (function($){
 $.fn.autosubmit = function(config) {
-  var Queue = (function() {
-    var q = [];
-    var push = function(func) {
-      if (typeof func !== 'function') return;
-      
-      q.push(function() {
-        func();
-        q.shift();
-        if (q.length) q[0]();
-      });
-      
-      if(q.length == 1) {
-        setTimeout(q[0], 1);
-      }
-    };
-    
-    return {
-      push: push
-    };
-  })();
-  
   return this.each(function() {
 
     var config = $.extend(config, {
-      intervalMillis: 3000
+      intervalMillis: 750
     });
     var form = $(this).parents('form');
     var timer = null;
     var startTimer = function() {
       if (timer) clearTimeout(timer);
       timer = setTimeout(function() {
-        Queue.push(function() { form.submit(); });
+        form.submit();
       }, config.intervalMillis);
     };
     
