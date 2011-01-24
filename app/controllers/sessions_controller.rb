d41a4577b82b1719a3a2cf16301f2e9d22ff055e
David Crawford <david.crawford@gmail.com>
2011-01-23 23:08:31 -0500

    Allow api login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   def new
     @title = "Sign in"
     store_location unless request.referrer =~ /signup|signin|sessions/
